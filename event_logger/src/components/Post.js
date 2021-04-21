073f0f203cda3f3d9850e46dceb1ed14f5fb4dd5
DanielAdeyemi <adeyemidany@gmail.com>
2021-04-21 14:38:34 -0700

    create post layout
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

--- event_logger/src/components/Post.js
+++ event_logger/src/components/Post.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-export default function Post() {
+export default function Post(props) {
   return(
     <>
+      <h2> {props.postTitle} </h2>
+      <h4><i> {props.postAuthor} </i></h4>
+      <p> {props.postContent} </p>
     </>
   );
 }
+
+Post.propTypes = {
+  postTitle: PropTypes.string.isRequired,
+  postAuthor: PropTypes.string,
+  postContent: PropTypes.string,
+  id: PropTypes.string
+};
